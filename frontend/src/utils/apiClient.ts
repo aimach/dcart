d1bd10175b5ed5a924da4f2a802cf13892ebe87f
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2025-01-07 14:40:02 +0100

    feat(frontend): add axios library in frontend and create instance

---
+++ frontend/src/utils/apiClient.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+	baseURL: `http://localhost:${import.meta.env.VITE_BACKEND_PORT}`,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
