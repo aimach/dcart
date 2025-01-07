66eb44d03b391fc2a7a30ca72831f0b57eed6e5c
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2025-01-07 16:36:21 +0100

    feat(frontend): add leaflet and leaflet-react libraries and display a map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import du style
 import "./App.css";
+// import des composants
+import MapComponent from "./components/MapComponent";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<div className="app">
+			<h1>My Leaflet Map</h1>
+			<MapComponent />
+		</div>
 	);
 }
 
