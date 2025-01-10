b5d6021b56e1725274584ad5b2a41149cdf1fbf0
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2025-01-10 16:10:52 +0100

    feat(map): add panels in map page
    Add left (results) and right (filters) panels in map - Add automatic reload when panels are closed

--- frontend/src/components/mapComponent/MapComponent.tsx
+++ frontend/src/components/mapComponent/MapComponent.tsx
@@ -1,16 +1,31 @@
 // import des bibliothèques
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import des types
+import type { Map as LeafletMap } from "leaflet";
 // import du style
 import "leaflet/dist/leaflet.css";
 import "./mapComponent.css";
 import style from "./mapComponent.module.scss";
 
-const MapComponent = () => {
+interface MapComponentProps {
+	toggleButtons: { [key: string]: boolean };
+}
+
+const MapComponent = ({ toggleButtons }: MapComponentProps) => {
+	const [map, setMap] = useState<LeafletMap | null>(null);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		if (map) {
+			(map as LeafletMap).invalidateSize();
+		}
+	}, [toggleButtons]);
+
 	return (
 		<div className="map" id="map">
 			<section className="leaflet-container">
-				<MapContainer center={[40.43, 16.52]} zoom={5} scrollWheelZoom={false}>
-					{/* Layer principal */}
+				<MapContainer center={[40.43, 16.52]} zoom={5} ref={setMap}>
 					<TileLayer
 						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 						url="https://cawm.lib.uiowa.edu/tiles/%7Bz%7D/%7Bx%7D/%7By%7D.png/tiles/{z}/{x}/{y}.png"
--- frontend/src/pages/MapPage/MapPage.tsx
+++ frontend/src/pages/MapPage/MapPage.tsx
@@ -1,5 +1,5 @@
 // import des bibliothèques
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 // import des composants
 import MapComponent from "../../components/mapComponent/MapComponent";
 // import du style
@@ -8,6 +8,10 @@ import LoaderComponent from "../../components/common/loader/LoaderComponent";
 
 const MapPage = () => {
 	const [mapReady, setMapReady] = useState<boolean>(false);
+	const [toggleButtons, setToggleButtons] = useState({
+		right: true,
+		left: true,
+	});
 
 	useEffect(() => {
 		// on attend que les tuiles soient chargées
@@ -18,11 +22,57 @@ const MapPage = () => {
 		<section className={style.mapSection}>
 			<header className={style.mapSectionHeader}>Menu</header>
 			<section className={style.mapSectionMain}>
-				<aside>Résultats</aside>
+				<aside
+					className={
+						toggleButtons.left ? `${style.asideOpened}` : `${style.asideClosed}`
+					}
+				>
+					Résultats
+					<div className={style.toggleButtonContainer}>
+						<button
+							type="button"
+							className={`${style.toggleButtonLeft} ${style.toggleButton}`}
+							onClick={() =>
+								setToggleButtons({
+									...toggleButtons,
+									left: !toggleButtons.left,
+								})
+							}
+						>
+							{toggleButtons.left ? "<" : ">"}
+						</button>
+					</div>
+				</aside>
 				<section className={mapReady ? undefined : style.mapSectionLoaded}>
-					{mapReady ? <MapComponent /> : <LoaderComponent />}
+					{mapReady ? (
+						<MapComponent toggleButtons={toggleButtons} />
+					) : (
+						<LoaderComponent />
+					)}
 				</section>
-				<aside>Filtres</aside>
+				<aside
+					className={
+						toggleButtons.right
+							? `${style.asideOpened}`
+							: `${style.asideClosed}`
+					}
+				>
+					Filtres
+					<div className={style.toggleButtonContainer}>
+						<button
+							type="button"
+							className={`${style.toggleButtonRight} ${style.toggleButton}`}
+							onClick={() =>
+								setToggleButtons({
+									...toggleButtons,
+									right: !toggleButtons.right,
+								})
+							}
+						>
+							{toggleButtons.right ? ">" : "<"}
+						</button>
+					</div>
+				</aside>
 			</section>
 		</section>
 	);
