0778985e72dcdf8d623067d229c9526deffa7738
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2025-01-14 11:05:21 +0100

    feat(datasource): add datasource for MPA DB

---
+++ backend/src/dataSource/dataSource.ts
@@ -0,0 +1,28 @@
+// import des bibliothèques
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+// on charge les variables d'environnement
+dotenv.config();
+
+export const dcartDataSource = new DataSource({
+	type: "postgres",
+	host: process.env.DCART_DB_HOST,
+	port: Number.parseInt(process.env.DCART_DB_PORT as string, 10),
+	username: process.env.DCART_DB_USERNAME,
+	password: process.env.DCART_DB_PASSWORD,
+	database: process.env.DCART_DB_NAME,
+	synchronize: true, // Crée automatiquement les tables
+	logging: false, // Active les logs pour le debug
+	entities: [`${__dirname}/../entities/*.ts`],
+});
+
+export const MapDataSource = new DataSource({
+	type: "postgres",
+	host: process.env.MAP_DB_HOST,
+	port: Number.parseInt(process.env.MAP_DB_PORT as string, 10),
+	username: process.env.MAP_DB_USERNAME,
+	password: process.env.MAP_DB_PASSWORD,
+	database: process.env.MAP_DB_NAME,
+	logging: false, // Active les logs pour le debug
+});
