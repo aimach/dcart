de005fbe6d6ac8491447ce0ecb9eac14f88d5f8d
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2025-01-15 14:04:23 +0100

    perf(types): add types for points : sources, attestations, elements

--- frontend/src/types/mapTypes.ts
+++ frontend/src/types/mapTypes.ts
@@ -15,4 +15,54 @@ type MapType = {
 	updatedAt: string;
 };
 
-export type { MapType };
+type ElementType = {
+	element_id: number;
+	etat_absolu: string;
+	element_nom_en: string;
+	element_nom_fr: string;
+};
+
+type AgentType = {
+	genres: [
+		{
+			nom_en: string;
+			nom_fr: string;
+		},
+	];
+	activite_en: string;
+	activite_fr: string;
+	designation: string;
+};
+
+type AttestationType = {
+	agents: AgentType[] | null;
+	nom_en: string;
+	nom_fr: string;
+	formule: string;
+	elements: ElementType[];
+	attestation_id: number;
+	translitteration: string;
+	extrait_avec_restitution: string;
+};
+
+type SourceType = {
+	ante_quem: number;
+	post_quem: number;
+	source_id: number;
+	support_en: string;
+	support_fr: string;
+	materiau_en: string;
+	materiau_fr: string;
+	attestations: AttestationType[];
+};
+
+type PointType = {
+	latitude: number;
+	longitude: number;
+	sous_region_fr: string;
+	sous_region_en: string;
+	nom_ville: string;
+	sources: SourceType[];
+};
+
+export type { MapType, PointType };
