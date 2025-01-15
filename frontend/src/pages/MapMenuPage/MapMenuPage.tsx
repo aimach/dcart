ff5d91481a82adc7f27fc092056dff74a8142646
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2025-01-15 10:06:04 +0100

    feat(frontend/map menu): add page with map menu

--- frontend/src/pages/MapMenuPage/MapMenuPage.tsx
+++ frontend/src/pages/MapMenuPage/MapMenuPage.tsx
@@ -1,20 +1,51 @@
+// import des bibliothèques
+import { useContext } from "react";
+import { Link } from "react-router";
+// import des composants
+import NavComponent from "../../components/common/NavComponent";
+// import du context
+import { TranslationContext } from "../../context/TranslationContext";
+// import des types
+import type { NavList } from "../../types/commonTypes";
 // import du style
 import style from "./mapMenuPage.module.scss";
 
 const MapMenuPage = () => {
+	// on récupère le language
+	const { language, translation } = useContext(TranslationContext);
+
+	// on prépare la liste des cartes
+	const thematicMapsList: NavList = [
+		{
+			title: "Carte thématique 1",
+			onClickFunction: undefined,
+			route: "/map/exploration",
+		},
+		{
+			title: "Carte thématique 2",
+			onClickFunction: undefined,
+			route: "/map/exploration",
+		},
+		{
+			title: "Carte thématique 3",
+			onClickFunction: undefined,
+			route: "/map/exploration",
+		},
+	];
+
 	return (
 		<section className={style.mapMenu}>
 			<div className={style.mapMenuButtonContainer}>
-				<button type="button" className={style.mapMenuActionButton}>
-					Explorer librement
-				</button>
+				<Link to="exploration" className={style.mapMenuActionButton}>
+					{translation[language].button.freeExploration}
+				</Link>
 			</div>
 			<div className={style.mapMenuNavContainer}>
-				<ul className={style.mapMenuNav}>
-					<li>Carte thématique 1</li>
-					<li>Carte thématique 1</li>
-					<li>Carte thématique 1</li>
-				</ul>
+				<NavComponent
+					type="route"
+					navClassName={style.mapMenuNav}
+					list={thematicMapsList}
+				/>
 			</div>
 		</section>
 	);
