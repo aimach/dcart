c5f76ba7fd600f8dc5e62cb56e17429c0abe4bbd
aimach <vigneron.mh@gmail.com>
2025-02-05 13:44:09 +0100

    feat(menu component): display menu component from header icon

--- frontend/src/layouts/NavigationLayout.tsx
+++ frontend/src/layouts/NavigationLayout.tsx
@@ -1,14 +1,21 @@
 // import des bibliothèques
+import { useState } from "react";
 import { Outlet } from "react-router";
 // import des composants
 import HeaderComponent from "../components/header/Header";
 // import du style
 import style from "./layout.module.scss";
+import AppMenuComponent from "../components/menu/AppMenuComponent";
 
 const NavigationLayout = () => {
-	return (
+	// on initie le state pour l'affichage du menu
+	const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+	console.log(menuIsOpen);
+	return menuIsOpen ? (
+		<AppMenuComponent setMenuIsOpen={setMenuIsOpen} />
+	) : (
 		<div className={style.layout}>
-			<HeaderComponent type={"visitor"} />
+			<HeaderComponent type={"visitor"} setMenuIsOpen={setMenuIsOpen} />
 			<main>
 				<Outlet />
 			</main>
