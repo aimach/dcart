f69a024fa03991f469dc52d2db5b7a6dc1ad10d6
Marie-Hermine Schneider <32041949+aimach@users.noreply.github.com>
2025-02-05 13:07:03 +0100

    Style/home page (#18)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,49 @@
 // import des bibliothèques
 import { useContext } from "react";
+// import des composants
+import NavComponent from "./components/common/NavComponent";
 // import du context
 import { TranslationContext } from "./context/TranslationContext";
 // import du style
-import "./App.module.scss";
+import style from "./App.module.scss";
 
 function App() {
 	const { language, translation } = useContext(TranslationContext);
 
+	const menuList = [
+		{
+			id: "maps",
+			title: translation[language].navigation.explore,
+			onClickFunction: undefined,
+			route: "maps/categories",
+		},
+		{
+			id: "storymaps",
+			title: translation[language].navigation.discover,
+			onClickFunction: undefined,
+			route: "/storymaps",
+		},
+	];
+
 	return (
-		<div className="app">
+		<section className={style.mainPage}>
 			<h1>{translation[language].title as string}</h1>
-		</div>
+			<div className={style.verticalSeparator} />
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, iure
+				amet tempore ut itaque dolor repellendus repellat officiis natus!
+				Aliquid ea voluptates suscipit. Odit numquam maxime, blanditiis magnam
+				atque sit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Animi dolorem laborum ratione adipisci consequuntur sapiente culpa
+				reiciendis quis, voluptatum quia, a aperiam velit nam! Dicta saepe
+				fugiat ad delectus sunt.
+			</p>
+			<NavComponent
+				type="route"
+				navClassName={style.homeMenu}
+				list={menuList}
+			/>
+		</section>
 	);
 }
 
