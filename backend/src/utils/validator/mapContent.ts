07d0729b1a7b70ee801e4db4f3ae6592235238cf
aimach <vigneron.mh@gmail.com>
2025-03-03 12:45:39 +0100

    feat(map content validation): add map content validation to create route

---
+++ backend/src/utils/validator/mapContent.ts
@@ -0,0 +1,59 @@
+// import des bibliothèques
+import Joi from "joi";
+// import des types
+import type { Request, Response, NextFunction } from "express";
+
+const mapContentSchema = Joi.object({
+	id: Joi.string().uuid().optional(),
+	name_fr: Joi.string().required().messages({
+		"any.required": "Le nom en français est requis",
+		"string.base": "Le nom en français doit être une chaîne de caractères",
+	}),
+	name_en: Joi.string().required().messages({
+		"any.required": "Le nom en anglais est requis",
+		"string.base": "Le nom en anglais doit être une chaîne de caractères",
+	}),
+	description_fr: Joi.string().optional().allow(null).messages({
+		"string.base":
+			"La description en français doit être une chaîne de caractères",
+	}),
+	description_en: Joi.string().optional().allow(null).messages({
+		"string.base":
+			"La description en anglais doit être une chaîne de caractères",
+	}),
+	attestationIds: Joi.string().required().messages({
+		"any.required": "La liste des id des attestations est requise",
+		"string.base":
+			"La liste des id des attestations doit être une chaîne de caractères",
+	}),
+	divinityIds: Joi.string().optional().allow("").allow(null),
+	isActive: Joi.boolean().required().messages({
+		"any.required": "Le booléen 'isActive' est requis",
+		"boolean.base": "'isActive' doit être un booléen",
+	}),
+	createdAt: Joi.date().optional(),
+	updatedAt: Joi.date().optional(),
+	category: Joi.object({
+		id: Joi.string().uuid().required(),
+	}).optional(),
+	filters: Joi.array()
+		.items(
+			Joi.object({
+				id: Joi.string().uuid().required(),
+			}),
+		)
+		.optional(),
+});
+
+export const validateMapContentBody = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const { error } = mapContentSchema.validate(req.body);
+	if (error) {
+		res.status(422).send({ erreur: error.details[0].message });
+	} else {
+		next();
+	}
+};
