f8a0465fcdeb04b0b99657532fe51740416c5cc4
aimach <vigneron.mh@gmail.com>
2025-03-24 17:18:42 +0100

    fix(display filters): remove time filters string if -1000 and +400

--- frontend/src/components/builtMap/map/mapTitleComponent/MapTitleComponent.tsx
+++ frontend/src/components/builtMap/map/mapTitleComponent/MapTitleComponent.tsx
@@ -1,10 +1,7 @@
 // import des custom hooks
 import { useTranslation } from "../../../../utils/hooks/useTranslation";
 // import des services
-import {
-	displayFiltersTags,
-	noUserFilterChecked,
-} from "../../../../utils/functions/filter";
+import { displayFiltersTags } from "../../../../utils/functions/filter";
 import { useMapFiltersStore } from "../../../../utils/stores/builtMap/mapFiltersStore";
 import { useMapStore } from "../../../../utils/stores/builtMap/mapStore";
 // import du style
@@ -35,7 +32,7 @@ const MapTitleComponent = () => {
 		<div className={style.mapTitleContainer}>
 			<h2>{mapInfos ? mapInfos[`title_${language}`] : "Exploration"}</h2>
 			<div>
-				{!noUserFilterChecked(userFilters) &&
+				{filtersDetails.length > 0 &&
 					`${translation[language].mapPage.aside.filters} : `}
 				<ul>
 					{filtersDetails.map((filter) => (
