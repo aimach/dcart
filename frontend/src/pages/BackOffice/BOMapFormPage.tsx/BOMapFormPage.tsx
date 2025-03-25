737c85d0763bca845db2e51fa178e102abb299d8
aimach <vigneron.mh@gmail.com>
2025-03-25 17:11:30 +0100

    feat(map form): modify style in map form : add aside menu + display inputs in row

--- frontend/src/pages/BackOffice/BOMapFormPage.tsx/BOMapFormPage.tsx
+++ frontend/src/pages/BackOffice/BOMapFormPage.tsx/BOMapFormPage.tsx
@@ -1,20 +1,21 @@
 // import des bibliothèques
 import { useContext, useEffect } from "react";
 import { useLocation } from "react-router";
+// import du contexte
+import { SessionContext } from "../../../context/SessionContext";
 // import des composants
 import IntroForm from "../../../components/form/mapForm/introForm/IntroForm";
 import DemoMapComponent from "../../../components/builtMap/map/demoMapComponent/DemoMapComponent";
 import UploadForm from "../../../components/form/mapForm/uploadForm/UploadForm";
 import UserMapFilterForm from "../../../components/form/mapForm/userMapFilterForm/UserMapFilterForm";
+import StayConnectedContent from "../../../components/common/modal/StayConnectedContent";
+import ModalComponent from "../../../components/common/modal/ModalComponent";
 // import des services
 import { firstStepInputs } from "../../../utils/forms/mapInputArray";
 import { useMapFormStore } from "../../../utils/stores/builtMap/mapFormStore";
+import { useModalStore } from "../../../utils/stores/storymap/modalStore";
 // import du style
 import style from "./BOMapFormPage.module.scss";
-import { SessionContext } from "../../../context/SessionContext";
-import ModalComponent from "../../../components/common/modal/ModalComponent";
-import { useModalStore } from "../../../utils/stores/storymap/modalStore";
-import StayConnectedContent from "../../../components/common/modal/StayConnectedContent";
 
 /**
  * Page du formulaire de création de carte
@@ -37,21 +38,39 @@ const BOMapFormPage = () => {
 	}, []);
 
 	return (
-		<>
+		<section className={style.BOmapFormPageContainer}>
 			{isTimeoutReached && (
 				<ModalComponent onClose={() => closeDeleteModal()} isDemo={false}>
 					<StayConnectedContent />
 				</ModalComponent>
 			)}
-			<section className={style.BOmapFormPageContainer}>
+			<aside className={style.mapFormAside}>
+				<ul>
+					<li onClick={() => setStep(1)} onKeyUp={() => setStep(1)}>
+						Informations
+					</li>
+					<li onClick={() => setStep(2)} onKeyUp={() => setStep(2)}>
+						Jeux de points
+					</li>
+					<li onClick={() => setStep(3)} onKeyUp={() => setStep(3)}>
+						Filtres associés
+					</li>
+				</ul>
+			</aside>
+			<section className={style.mapFormContent}>
+				{step === 1 && <IntroForm inputs={firstStepInputs} />}
+				{step === 2 && <UploadForm />}
+				{step === 3 && <UserMapFilterForm />}
+			</section>
+			{/* <section className={style.BOmapFormPageContainer}>
 				<div>
 					{step === 1 && <IntroForm inputs={firstStepInputs} />}
 					{step === 2 && <UploadForm />}
 					{step === 3 && <UserMapFilterForm />}
 				</div>
 				<DemoMapComponent showModal={step === 1} />
-			</section>
-		</>
+			</section> */}
+		</section>
 	);
 };
 
