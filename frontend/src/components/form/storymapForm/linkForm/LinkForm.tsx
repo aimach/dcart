eb6fdf597590626d1877f3ebbe7bee884c0dd66d
aimach <vigneron.mh@gmail.com>
2025-04-02 16:24:50 +0200

    feat(link): modify link form with 2 new inputs (lang2 and text)

--- frontend/src/components/form/storymapForm/linkForm/LinkForm.tsx
+++ frontend/src/components/form/storymapForm/linkForm/LinkForm.tsx
@@ -14,10 +14,7 @@ import {
 } from "../../../../utils/api/storymap/postRequests";
 // import des types
 import type { SubmitHandler } from "react-hook-form";
-import type {
-	BlockContentType,
-	StorymapType,
-} from "../../../../utils/types/storymapTypes";
+import type { BlockContentType } from "../../../../utils/types/storymapTypes";
 import type { allInputsType } from "../../../../utils/types/formTypes";
 import {
 	notifyCreateSuccess,
