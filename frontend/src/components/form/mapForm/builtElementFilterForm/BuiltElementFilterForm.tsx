600b0c12324a1c327676e32acf067e34557fad2e
aimach <vigneron.mh@gmail.com>
2025-04-07 14:20:29 +0200

    feat(solution form): add 2 radio buttons to choose between automatic or manual solution

--- frontend/src/components/form/mapForm/builtElementFilterForm/BuiltElementFilterForm.tsx
+++ frontend/src/components/form/mapForm/builtElementFilterForm/BuiltElementFilterForm.tsx
@@ -4,10 +4,68 @@
 // import des services
 // import des types
 // import du style
-import style from "./BuiltElementFilterForm.module.scss";
+import { useTranslation } from "../../../../utils/hooks/useTranslation";
+import style from "../introForm/introForm.module.scss";
 
 const BuiltElementFilterForm = () => {
-	return <div>BuiltElementFilterForm</div>;
+	const { translation, language } = useTranslation();
+
+	const handleSubmit = () => {
+		console.log("Form submitted");
+	};
+
+	const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { checked } = event.target;
+		console.log("Checkbox checked:", checked);
+		// Handle the checkbox change logic here
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className={style.commonFormContainer}>
+			{/* <h4>{translation[language].backoffice.mapFormPage.addFilters}</h4> */}
+			<h4>Construction du filtre "Elements"</h4>
+			<div className={style.commonFormInputContainer}>
+				<div className={style.labelContainer}>
+					<label htmlFor="automatic">Automatique</label>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+						dignissimos, suscipit beatae, soluta accusamus, et voluptatem velit
+						officiis facilis ratione mollitia sed repudiandae? Eum vel tenetur,
+						dolores quasi vitae perferendis!
+					</p>
+				</div>
+				<div className={style.inputContainer}>
+					<input
+						id="automatic"
+						name="builtSolution"
+						type="radio"
+						checked=""
+						onChange={(event) => handleRadioChange(event)}
+					/>
+				</div>
+			</div>
+			<div className={style.commonFormInputContainer}>
+				<div className={style.labelContainer}>
+					<label htmlFor="manual">Manuelle</label>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+						dignissimos, suscipit beatae, soluta accusamus, et voluptatem velit
+						officiis facilis ratione mollitia sed repudiandae? Eum vel tenetur,
+						dolores quasi vitae perferendis!
+					</p>
+				</div>
+				<div className={style.inputContainer}>
+					<input
+						id="manual"
+						name="builtSolution"
+						type="radio"
+						checked=""
+						onChange={(event) => handleRadioChange(event)}
+					/>
+				</div>
+			</div>
+		</form>
+	);
 };
 
 export default BuiltElementFilterForm;
