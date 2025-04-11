1ef54930028203fb0dfb6d4d15e03c06163b8f68
aimach <vigneron.mh@gmail.com>
2025-04-11 13:59:27 +0200

    feat(routes): writer cannot access to admin pages by URL

--- frontend/src/pages/BackOffice/BOTranslationPage/BackofficeTranslationPage.tsx
+++ frontend/src/pages/BackOffice/BOTranslationPage/BackofficeTranslationPage.tsx
@@ -1,4 +1,18 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import { useNavigate } from "react-router";
+
 const BackofficeTranslationPage = () => {
+	const { isAdmin } = useContext(AuthContext);
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isAdmin) {
+			navigate("/backoffice");
+		}
+	}, [isAdmin, navigate]);
+
 	return <div>Page de la traduction</div>;
 };
 
