716481b5c03b59570886a34ebb73209d49dda0fb
aimach <vigneron.mh@gmail.com>
2025-04-28 15:44:09 +0200

    style(global): add style to user management page and table

--- frontend/src/pages/BackOffice/BOUserManagementPage/BOUserManagementPage.tsx
+++ frontend/src/pages/BackOffice/BOUserManagementPage/BOUserManagementPage.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router";
 import LoaderComponent from "../../../components/common/loader/LoaderComponent";
 import ModalComponent from "../../../components/common/modal/ModalComponent";
 import DeleteUserContent from "../../../components/common/modal/DeleteUserContent";
+import UpdateUserStatusContent from "../../../components/common/modal/UpdateUserStatusContent";
 // import du context
 import { AuthContext } from "../../../context/AuthContext";
 // import des services
@@ -12,9 +13,10 @@ import { getAllUsers } from "../../../utils/api/common/getRequests";
 import { useModalStore } from "../../../utils/stores/storymap/modalStore";
 // import des types
 import type { User } from "../../../utils/types/userTypes";
+// import des styles
+import style from "./userManagementPage.module.scss";
 // import des icônes
 import { Trash } from "lucide-react";
-import UpdateUserStatusContent from "../../../components/common/modal/UpdateUserStatusContent";
 
 const UserManagementPage = () => {
 	const { isAdmin } = useContext(AuthContext);
@@ -64,7 +66,7 @@ const UserManagementPage = () => {
 	};
 
 	return users.length > 0 ? (
-		<section>
+		<section className={style.userManagementSection}>
 			{isDeleteModalOpen && (
 				<ModalComponent onClose={() => closeDeleteModal()}>
 					<DeleteUserContent />
@@ -76,43 +78,45 @@ const UserManagementPage = () => {
 				</ModalComponent>
 			)}
 			<h4>Gestion des utilisateurs</h4>
-			<table>
-				<thead>
-					<tr>
-						<th>Nom d'utilisateur</th>
-						<th>Pseudo</th>
-						<th>Rôle</th>
-						<th>Liens rapides</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user) => (
-						<tr key={user.id}>
-							<td>{user.username}</td>
-							<td>{user.pseudo}</td>
-							<td>{user.status}</td>
-							<td>
-								{userId !== user.id && (
-									<>
-										<button
-											type="button"
-											onClick={() => handleStatusClick(user.id as string)}
-										>
-											{user.status === "admin"
-												? "Passer en auteur"
-												: "Passer en admin"}
-										</button>
-										<Trash
-											color="#9d2121"
-											onClick={() => handleDeleteClick(user.id as string)}
-										/>
-									</>
-								)}
-							</td>
+			<div className={style.userManagementTableContainer}>
+				<table className={style.managementTable}>
+					<thead>
+						<tr>
+							<th>Nom d'utilisateur</th>
+							<th>Pseudo</th>
+							<th>Rôle</th>
+							<th>Liens rapides</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{users.map((user) => (
+							<tr key={user.id} className={style.userTableRow}>
+								<td>{user.username}</td>
+								<td>{user.pseudo}</td>
+								<td>{user.status}</td>
+								<td>
+									{userId !== user.id && (
+										<>
+											<button
+												type="button"
+												onClick={() => handleStatusClick(user.id as string)}
+											>
+												{user.status === "admin"
+													? "Passer en auteur"
+													: "Passer en admin"}
+											</button>
+											<Trash
+												color="#9d2121"
+												onClick={() => handleDeleteClick(user.id as string)}
+											/>
+										</>
+									)}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</section>
 	) : (
 		<LoaderComponent size={40} />
