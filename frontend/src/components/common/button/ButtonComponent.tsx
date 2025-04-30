b182f6a0b274551bf6469a6ff149aab6e20ea096
aimach <vigneron.mh@gmail.com>
2025-04-30 10:26:26 +0200

    feat(button component): add icon and new type (submit) to button component

--- frontend/src/components/common/button/ButtonComponent.tsx
+++ frontend/src/components/common/button/ButtonComponent.tsx
@@ -4,12 +4,13 @@ import { Link } from "react-router";
 import style from "./buttonComponent.module.scss";
 
 interface ButtonComponentProps {
-	type: "route" | "button";
+	type: "route" | "button" | "submit";
 	color: "gold" | "brown" | "red";
 	textContent: string;
 	onClickFunction?: () => void;
 	link?: string;
 	isSelected?: boolean;
+	icon?: React.ReactNode;
 }
 
 /**
@@ -20,6 +21,7 @@ interface ButtonComponentProps {
  * @param {function} onClickFunction - Fonction à exécuter au clic
  * @param {string} link - Si de type "route", lien vers lequel rediriger
  * @param {boolean} isSelected - Si le bouton est sélectionné
+ * @param {string} icon - Icône à afficher dans le bouton (optionnel)
  */
 const ButtonComponent = ({
 	type,
@@ -28,23 +30,42 @@ const ButtonComponent = ({
 	onClickFunction,
 	link,
 	isSelected = true,
+	icon,
 }: ButtonComponentProps) => {
-	return type === "route" ? (
-		<Link
-			to={link as string}
-			className={`${style.simpleButton} ${isSelected ? style[color] : style.unselected}`}
-		>
-			{textContent}
-		</Link>
-	) : (
-		<button
-			type="button"
-			onClick={onClickFunction}
-			className={`${style.simpleButton} ${isSelected ? style[color] : style.unselected}`}
-		>
-			{textContent}
-		</button>
-	);
+	if (type === "route") {
+		return (
+			<Link
+				to={link as string}
+				className={`${style.simpleButton} ${isSelected ? style[color] : style.unselected}`}
+			>
+				{textContent}
+			</Link>
+		);
+	}
+
+	if (type === "button") {
+		return (
+			<button
+				type="button"
+				onClick={onClickFunction}
+				className={`${style.simpleButton} ${isSelected ? style[color] : style.unselected}`}
+			>
+				{icon ?? ""} {textContent}
+			</button>
+		);
+	}
+
+	if (type === "submit") {
+		return (
+			<button
+				type="submit"
+				onClick={onClickFunction}
+				className={`${style.simpleButton} ${isSelected ? style[color] : style.unselected}`}
+			>
+				{icon ?? ""} {textContent}
+			</button>
+		);
+	}
 };
 
 export default ButtonComponent;
