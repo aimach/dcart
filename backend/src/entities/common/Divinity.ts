66ae32545bbcc94c4f0eea997c750a04f268a6c0
aimach <vigneron.mh@gmail.com>
2025-05-12 09:19:53 +0200

    feat(entity): add default value with divinity list for divinity entity

--- backend/src/entities/common/Divinity.ts
+++ backend/src/entities/common/Divinity.ts
@@ -1,8 +1,10 @@
 // import des bibliothèques
 import { Entity, BaseEntity, PrimaryColumn } from "typeorm";
+// import des données
+import divinityList from "../../utils/divinityList/divinityList";
 
 @Entity()
 export class Divinity extends BaseEntity {
-	@PrimaryColumn({ type: "text" })
+	@PrimaryColumn({ type: "text", default: divinityList })
 	divinity_list?: string;
 }
