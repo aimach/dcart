87d9b407ca827d768d27e576534f7e7e6122519f
aimach <vigneron.mh@gmail.com>
2025-07-17 13:37:48 +0200

    feat(translation): add EN translation to legal notice page

--- frontend/src/pages/Legal/LegalNoticePage.tsx
+++ frontend/src/pages/Legal/LegalNoticePage.tsx
@@ -1,14 +1,18 @@
-// import du style
+// import des composants
 import { LegalNoticePageHelmetContent } from "../../components/helmet/HelmetContent";
+// import des custom hooks
+import { useTranslation } from "../../utils/hooks/useTranslation";
+// import du style
 import style from "./legalPage.module.scss";
 
 const LegalNoticePage = () => {
-	return (
+	const { language } = useTranslation();
+	return language === "fr" ? (
 		<>
 			<LegalNoticePageHelmetContent />
 			<div className={style.legalPage}>
 				<h1>Crédits et Mentions légales</h1>
-				<p>Dernière mise à jour : 10 juillet 2025</p>
+				<p>Dernière mise à jour : 17 juillet 2025</p>
 				<div className={style.paragraphContainer}>
 					<h2>Informations éditoriales</h2>
 					<p>
@@ -51,7 +55,7 @@ const LegalNoticePage = () => {
 					</p>
 					<p>
 						<h3>Crédits images, multimédia</h3>
-						Photos : Les crédits mentionnés dans les pages <br />
+						Photos : Credits indicated on the respective pages <br />
 						Vidéos : crédits mentionnés dans les pages
 					</p>
 					<p>
@@ -181,6 +185,181 @@ const LegalNoticePage = () => {
 							social de l’éditeur, sous réserve d’une attribution de compétence
 							spécifique découlant d’un texte de loi ou réglementaire
 							particulier.
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	) : (
+		<>
+			<LegalNoticePageHelmetContent />
+			<div className={style.legalPage}>
+				<h1>Credits and Legal Notice</h1>
+				<p>Last updated: July 17, 2025</p>
+				<div className={style.paragraphContainer}>
+					<h2>Editorial Information</h2>
+					<p>
+						This website is published as part of the dCART project (des dieux à
+						la carte,{" "}
+						<a href="https://sms.univ-tlse2.fr/accueil-sms/la-recherche/operations-structurantes/dcart-des-dieux-a-la-carte">
+							link
+						</a>
+						), a research project funded by LabEx SMS (Sociétés, Milieux,
+						Santé). The aim of this project is to disseminate the data and
+						knowledge produced within the{" "}
+						<a href="https://base-map-polytheisms.huma-num.fr/">ERC MAP</a>
+						research project to a non-specialist audience. It harnesses the
+						possibilities of the web to share engaging content (Digital Atlas of
+						Divine Names in the Mediterranean).
+						<br />
+					</p>
+					<p>
+						<h3>Publication Director</h3>
+						<p>Corinne Bonnet</p>
+					</p>
+				</div>
+				<div className={style.paragraphContainer}>
+					<h2>Site Design, Development, and Hosting</h2>
+					<p>
+						<h3>Website Design</h3>
+						Marie-Hermine Schneider <br />
+						Graphic and ergonomic design, integration, and development
+						<br />
+						<br />
+						<a
+							href="https://github.com/aimach"
+							target="_blank"
+							rel="noreferrer"
+						>
+							github.com/aimach
+						</a>
+						<br />
+					</p>
+					<p>
+						<h3>Image and Multimedia Credits</h3>
+						Photos : credits indicated on the respective pages <br />
+						Vidéos : credits indicated on the respective pages
+					</p>
+					<p>
+						<h3>Hosting</h3>
+						<a href="https://www.huma-num.fr/" target="_blank" rel="noreferrer">
+							TGIR Huma-Num
+						</a>
+					</p>
+				</div>
+
+				<div className={style.paragraphContainer}>
+					<h2>Personal Data Processing</h2>
+					<p>
+						The team behind the website
+						<a href="https://dcart-map.huma-num.fr/">
+							https://dcart-map.huma-num.fr/
+						</a>
+						is particularly attentive to the protection of personal data. <br />
+						<a href="https://dcart-map.huma-num.fr/">
+							https://dcart-map.huma-num.fr/
+						</a>
+						is committed to implementing appropriate measures to ensure the
+						protection, confidentiality, and security of Personal Data in
+						accordance with the applicable regulations in France and the
+						European Union, in particular the General Data Protection Regulation
+						(EU) 2016/679 of April 27, 2016, and the national legal provisions
+						adopted for its implementation. <br /> For more information on
+						personal data protection, we invite you to visit the following
+						website:
+						<a href="https://www.cnil.fr/" target="_blank" rel="noreferrer">
+							www.cnil.fr
+						</a>
+						<br />
+					</p>
+					<p>
+						<h3>Data Collection and Origin</h3>
+						<p>
+							Through this website, we process personal data concerning you. We
+							consider that this data originates directly from you. In all
+							cases, you are informed of the purposes for which your data is
+							collected by us, either via the various online data collection
+							forms or through our Cookie Policy. <br /> The personal data
+							collected on this site is processed in accordance with the
+							purposes defined at the time of collection, in compliance with the
+							GDPR and the guidelines of the CNIL.
+						</p>
+					</p>
+					<p>
+						<h3>Purposes of Processing and Legal Bases</h3>
+						<h4>Website Login</h4>
+						<p>
+							When you log in to our website, certain personal data may be
+							processed as part of this activity, particularly in order to:
+							<br />– allow access to your user account; <br />– ensure secure
+							authentication and session management; <br />– provide you with
+							personalized services related to your profile. <br /> Processing
+							is based on the performance of a contract (access to your personal
+							space) and, where applicable, on our legitimate interest in
+							ensuring the security of our website. <br /> You may, at any time,
+							request: <br />– the modification of your account data (name,
+							email); <br />– the complete deletion of your account, which will
+							result in the erasure of your associated personal data, unless
+							legal obligations require retention.
+						</p>
+						<h4>Publications</h4>
+						<p>
+							Lorsque vous publiez une carte ou une storymap via notre site,
+							certaines données personnelles peuvent être rendues publiques,
+							notamment votre nom et prénom, en tant qu’auteur ou autrice.
+							<br />
+							Ces données sont traitées dans le but de : <br />- afficher
+							l’auteur de la carte publiée <br />- valoriser la contribution
+							individuelle des utilisateurs <br />- assurer la traçabilité
+							éditoriale des contenus proposés sur la plateforme
+							<br />
+							<br />
+							Le traitement repose sur votre consentement, donné au moment de la
+							création de la carte ou storymap. Vous pouvez, à tout moment,
+							demander :<br /> - la modification de vos données affichées,
+							<br />- leur suppression si vous ne souhaitez plus qu’elles soient
+							visibles publiquement.
+						</p>
+					</p>
+					<div className={style.paragraphContainer}>
+						<h2>Cookies</h2>
+						<p>
+							“Cookies” are small text files of limited size that allow us to
+							recognize your computer, tablet, or mobile device in order to
+							personalize the services we offer you.
+						</p>
+						<p>
+							We only use cookies that are strictly necessary for the proper
+							functioning of the website. No advertising or audience measurement
+							cookies are used.
+						</p>
+						<h4>Authentication Cookie</h4>
+						<p>
+							When you log in to your personal space, a cookie is created to
+							store an authentication token. This cookie keeps your session
+							active and secure. This cookie: <br />– is essential for the login
+							system to function properly; <br />– is stored locally in your
+							browser in a secure format; <br />– is never used for advertising
+							or tracking purposes. <br />
+							<br /> The legal basis for this processing is the performance of a
+							contract (access to your account) and our legitimate interest in
+							ensuring secure connections.
+						</p>
+						<p>
+							Most web browsers are set by default to allow the placement of
+							cookies. Your browser gives you the option to change these default
+							settings so that all cookies are rejected systematically, or only
+							certain cookies are accepted or rejected depending on their
+							source.
+						</p>
+					</div>
+					<div className={style.paragraphContainer}>
+						<h2>Applicable Law</h2>
+						<p>
+							These terms of use are governed by French law and fall under the
+							jurisdiction of the courts located at the publisher’s registered
+							office, subject to any specific jurisdiction assigned by a
+							particular legal or regulatory provision.
 						</p>
 					</div>
 				</div>
