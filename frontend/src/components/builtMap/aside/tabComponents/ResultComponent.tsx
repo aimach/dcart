4fd84674fa8bdaf00cd4a7a3cce932ed32369938
aimach <vigneron.mh@gmail.com>
2025-07-31 14:24:12 +0200

    fix(clean): remove useless code

--- frontend/src/components/builtMap/aside/tabComponents/ResultComponent.tsx
+++ frontend/src/components/builtMap/aside/tabComponents/ResultComponent.tsx
@@ -18,132 +18,132 @@ import style from "./tabComponent.module.scss";
  * Affiche une liste des points affichés sur la carte (filtrés ou non)
  */
 const ResultComponent = () => {
-	// récupération des données de traduction
-	const { language } = useTranslation();
+  // récupération des données de traduction
+  const { language } = useTranslation();
 
-	// récupération des données des stores
-	const {
-		mapInfos,
-		allResults,
-		allLayers,
-		selectedMarker,
-		setSelectedMarker,
-		hasGrayScale,
-	} = useMapStore(
-		useShallow((state) => ({
-			mapInfos: state.mapInfos,
-			allResults: state.allResults,
-			allLayers: state.allLayers,
-			selectedMarker: state.selectedMarker,
-			setSelectedMarker: state.setSelectedMarker,
-			hasGrayScale: state.hasGrayScale,
-		})),
-	);
-	const { setSelectedTabMenu } = useMapAsideMenuStore(
-		useShallow((state) => ({
-			setSelectedTabMenu: state.setSelectedTabMenu,
-		})),
-	);
+  // récupération des données des stores
+  const {
+    mapInfos,
+    allResults,
+    allLayers,
+    selectedMarker,
+    setSelectedMarker,
+    hasGrayScale,
+  } = useMapStore(
+    useShallow((state) => ({
+      mapInfos: state.mapInfos,
+      allResults: state.allResults,
+      allLayers: state.allLayers,
+      selectedMarker: state.selectedMarker,
+      setSelectedMarker: state.setSelectedMarker,
+      hasGrayScale: state.hasGrayScale,
+    }))
+  );
+  const { setSelectedTabMenu } = useMapAsideMenuStore(
+    useShallow((state) => ({
+      setSelectedTabMenu: state.setSelectedTabMenu,
+    }))
+  );
 
-	// fonction pour gérer le clic sur un point (zoom et passage à l'onglet "sélection")
-	const handleResultClick = useCallback(
-		(result: PointType) => {
-			setSelectedMarker(result);
-			setSelectedTabMenu("infos");
-		},
-		[setSelectedMarker, setSelectedTabMenu],
-	);
+  // fonction pour gérer le clic sur un point (zoom et passage à l'onglet "sélection")
+  const handleResultClick = useCallback(
+    (result: PointType) => {
+      setSelectedMarker(result);
+      setSelectedTabMenu("infos");
+    },
+    [setSelectedMarker, setSelectedTabMenu]
+  );
 
-	const filteredResultsWithSelectedPoint = useMemo(() => {
-		// filtre les points qui ne sont pas dans les calques sélectionnés
-		const allResultsFiltered = mapInfos?.isLayered
-			? allResults.filter((point: PointType) =>
-					mapInfos
-						? allLayers.some(
-								(string) =>
-									string.includes(`svg> ${point.layerNamefr}`) ||
-									string.includes(`svg> ${point.layerNameen}`),
-							)
-						: point,
-				)
-			: allResults;
-		// ajoute la classe "isSelected" aux points sélectionnés
-		const allResultsFilteredWithCSS = allResultsFiltered.map(
-			(point: PointType) => {
-				const isSelected = isSelectedMarker(selectedMarker as PointType, point);
-				return {
-					...point,
-					isSelected,
-					selectedClassName: isSelected ? style.isSelected : undefined,
-					shapeCode: DOMPurify.sanitize(
-						getShapeForLayerName(
-							point.shape as string,
-							point.color,
-							hasGrayScale,
-						),
-					),
-				};
-			},
-		);
+  const filteredResultsWithSelectedPoint = useMemo(() => {
+    // filtre les points qui ne sont pas dans les calques sélectionnés
+    const allResultsFiltered = mapInfos?.isLayered
+      ? allResults.filter((point: PointType) =>
+          mapInfos
+            ? allLayers.some(
+                (string) =>
+                  string.includes(`svg> ${point.layerNamefr}`) ||
+                  string.includes(`svg> ${point.layerNameen}`)
+              )
+            : point
+        )
+      : allResults;
+    // ajoute la classe "isSelected" aux points sélectionnés
+    const allResultsFilteredWithCSS = allResultsFiltered.map(
+      (point: PointType) => {
+        const isSelected = isSelectedMarker(selectedMarker as PointType, point);
+        return {
+          ...point,
+          isSelected,
+          selectedClassName: isSelected ? style.isSelected : undefined,
+          shapeCode: DOMPurify.sanitize(
+            getShapeForLayerName(
+              point.shape as string,
+              point.color,
+              hasGrayScale
+            )
+          ),
+        };
+      }
+    );
 
-		// trie les résultats par sous-région puis par nom de site
-		const allResultsInAlphaOrder = allResultsFilteredWithCSS.sort((a, b) => {
-			const subRegionA = a[`sous_region_${language}`] || "";
-			const subRegionB = b[`sous_region_${language}`] || "";
-			if (subRegionA < subRegionB) return -1;
-			if (subRegionA > subRegionB) return 1;
+    // trie les résultats par sous-région puis par nom de site
+    const allResultsInAlphaOrder = allResultsFilteredWithCSS.sort((a, b) => {
+      const subRegionA = a[`sous_region_${language}`] || "";
+      const subRegionB = b[`sous_region_${language}`] || "";
+      if (subRegionA < subRegionB) return -1;
+      if (subRegionA > subRegionB) return 1;
 
-			const cityA = a.nom_ville || "";
-			const cityB = b.nom_ville || "";
-			if (cityA < cityB) return -1;
-			if (cityA > cityB) return 1;
+      const cityA = a.nom_ville || "";
+      const cityB = b.nom_ville || "";
+      if (cityA < cityB) return -1;
+      if (cityA > cityB) return 1;
 
-			return 0;
-		});
-		return allResultsInAlphaOrder;
-	}, [allResults, selectedMarker, allLayers, mapInfos, language, hasGrayScale]);
+      return 0;
+    });
+    return allResultsInAlphaOrder;
+  }, [allResults, selectedMarker, allLayers, mapInfos, language, hasGrayScale]);
 
-	// zoom sur le point sélectionné
-	const isSelectedRef = useRef<HTMLDivElement | null>(null);
-	useEffect(() => {
-		if (isSelectedRef.current) {
-			isSelectedRef.current.scrollIntoView({
-				behavior: "instant",
-				block: "start",
-			});
-		}
-	}, []);
+  // zoom sur le point sélectionné
+  const isSelectedRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (isSelectedRef.current) {
+      isSelectedRef.current.scrollIntoView({
+        behavior: "instant",
+        block: "start",
+      });
+    }
+  }, []);
 
-	return (
-		<div
-			className={style.resultContainer}
-			key={filteredResultsWithSelectedPoint.length}
-			aria-label="Liste des points de la carte"
-		>
-			{filteredResultsWithSelectedPoint.map((result: PointType, index) => {
-				return (
-					<div
-						key={`${result.latitude}-${result.longitude}-$${index}`}
-						onClick={() => handleResultClick(result)}
-						onKeyUp={() => handleResultClick(result)}
-						className={`${style.resultDetails} ${result.selectedClassName}`}
-						ref={result.isSelected ? isSelectedRef : null}
-					>
-						<div
-							// biome-ignore lint/security/noDangerouslySetInnerHtml: sanitized
-							dangerouslySetInnerHTML={{ __html: result.shapeCode as string }}
-						/>
+  return (
+    <div
+      className={style.resultContainer}
+      key={filteredResultsWithSelectedPoint.length}
+      aria-label="Liste des points de la carte"
+    >
+      {filteredResultsWithSelectedPoint.map((result: PointType, index) => {
+        return (
+          <div
+            key={`${result.latitude}-${result.longitude}-$${index}`}
+            onClick={() => handleResultClick(result)}
+            onKeyUp={() => handleResultClick(result)}
+            className={`${style.resultDetails} ${result.selectedClassName}`}
+            ref={result.isSelected ? isSelectedRef : null}
+          >
+            <div
+              // biome-ignore lint/security/noDangerouslySetInnerHtml: sanitized
+              dangerouslySetInnerHTML={{ __html: result.shapeCode as string }}
+            />
 
-						<p>
-							{result.nom_ville} ({result[`sous_region_${language}`]}) -{" "}
-							{result.sources.length}{" "}
-							{result.sources.length > 1 ? "sources" : "source"}
-						</p>
-					</div>
-				);
-			})}
-		</div>
-	);
+            <p>
+              {result.nom_ville} ({result[`sous_region_${language}`]}) -{" "}
+              {result.sources.length}{" "}
+              {result.sources.length > 1 ? "sources" : "source"}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ResultComponent;
