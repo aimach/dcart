1369f462250add0f43130d7344ce3e1c64fb6dfe
Marie-Hermine Schneider <32041949+aimach@users.noreply.github.com>
2025-11-02 17:58:59 +0100

    Feat (homepage): add new text with style (#180)

---
+++ frontend/src/components/homePage/HomePageDescription.tsx
@@ -0,0 +1,180 @@
+import { useTranslation } from "../../utils/hooks/useTranslation";
+
+function HomePageDescription() {
+  const { language } = useTranslation();
+  return language === "en" ? (
+    <>
+      <p>
+        The religions of the ancient Mediterranean world were characterized by a
+        multiplicity of divine entities, each of which could be designated and
+        described by a variety of names and epithets. These complex systems,
+        commonly referred to as “pantheons”, were highly sensitive to historical
+        contexts (places, periods, landscapes, societies), and thus far from
+        being static structures, they were instead flexible and ever-changing
+        configurations.
+        <br />
+        <br />
+        Between 2017 and 2023, at the University of Toulouse – Jean Jaurès, a
+        research team led by Corinne Bonnet, now Professor at the Scuola Normale
+        Superiore in Pisa, explored the vast galaxy of names and epithets
+        attributed to deities in inscriptions, that is, texts engraved on stone,
+        metal, ivory, bone, and other materials. Funded by the European Research
+        Council (ERC), the research project MAP “Mapping Ancient Polytheisms.
+        Cult Epithets as an Interface between Religious Systems and Human
+        Agency” (
+        <a href="https://map-polytheisms.huma-num.fr/?lang=en">
+          https://map-polytheisms.huma-num.fr/?lang=en
+        </a>
+        ) has so far collected and recorded over 27,000 attestations of divine
+        names in Greek and Semitic languages (Hebrew, Phoenician, Punic,
+        Aramaic), found throughout the Mediterranean basin, from Iran to
+        Britain, from the Danube to the Nile, covering the period between 1000
+        BCE and 400 CE.
+        <br />
+        <br />
+        These names, or divine onomastic sequences, vary in length, from one or
+        two elements up to as many as 143 in the longest sequence. They may
+        refer to a single deity or to several, up to twenty-nine! This reveals a
+        highly rich and diversified, hence complex, linguistic universe. Some
+        elements are widespread and commonly shared, such as “great,” “lord,” or
+        “savior”, while others belong exclusively to a specific deity. The names
+        sketch fascinating portraits of the gods: their functions and modes of
+        action, the places they frequent, the objects or animals associated with
+        them, and much more.
+      </p>
+      <p>
+        Accessible to everyone and bilingual in French and English, the MAP
+        database (
+        <a href="https://base-map-polytheisms.huma-num.fr/">
+          https://base- map-polytheisms.huma-num.fr/
+        </a>
+        ) is a unique tool worldwide. It showcases the extraordinary richness of
+        polytheisms—and even of monotheism—from the perspective of divine
+        onomastics, that is, the ways in which gods were named through proper
+        names, adjectives, nouns, verbal or adverbial forms, balancing respect
+        for tradition with the capacity for innovation. Have you ever heard of
+        “Aphrodite, Guide of the People”, “Dionysos, Luxuriant Shepherd”, or
+        “King Hoter Miskar, Prince of Days, Master of Terror”?
+        <br />
+        <br />
+        The Digital Atlas of Divine Names in the Mediterranean is designed to
+        help you explore the vibrant world of ancient gods and goddesses. It
+        offers <strong>two main functionalities:</strong>
+        <br />
+        <ul>
+          <li>
+            <strong>"Explore"</strong>, which allows you to access a map
+            containing all the divine names from the MAP database and to
+            personalize your data visualization through a system of filters. You
+            can display documents referring to a specific place, period,
+            onomastic element, or the association of two elements… In short, you
+            can explore
+          </li>
+          <li>
+            <strong>"Discover"</strong>, which provides access to
+            pre-constructed maps designed to visualize particular phenomena or
+            processes, as well as cartographic narratives (<em>storymaps</em>)
+            combining text, maps, and images to tell stories about aspects of
+            the divine world as reflected in names… A particular deity, a
+            sanctuary, a function, or certain attributes. This section is
+            organized by thematic categories (“tags”), while keywords make it
+            easy to navigate among the available <em>storymaps</em>. Since we
+            aim to reach an international audience, these narratives may be
+            available in French, English, and Italian.
+          </li>
+        </ul>
+      </p>
+    </>
+  ) : (
+    <>
+      <p>
+        Les religions du monde méditerranéen antique se caractérisent par une
+        multiplicité d’entités divines, chacune d’entre elles susceptible d’être
+        désignée et caractérisée par une pluralité de noms et d’épithètes. Ces
+        systèmes complexes, que l’on appelle usuellement « panthéons », sont
+        très sensibles aux contextes historiques – lieux, époques, paysages,
+        sociétés – de sorte qu’ils ne constituent en aucune façon des structures
+        figées, mais tout à l’inverse des configurations changeantes et souples.
+        <br />
+        <br />
+        Entre 2017 et 2023, à l’université Toulouse – Jean Jaurès, une équipe de
+        recherche conduite par Corinne Bonnet, actuellement Professeur à l’Ecole
+        Normale Supérieure de Pise, a exploré la galaxie des noms et des
+        épithètes attribués aux divinités dans les inscriptions, c’est-à-dire
+        les textes gravés sur pierre, métal, ivoire, os… Soutenu par des fonds
+        de la Commission Européenne de la Recherche (ERC), le projet de
+        recherche MAP, « Mapping Ancient Polytheims. Cult Epithets as an
+        Interface between Religious Systems and Human Agency » (
+        <a href="https://map-polytheisms.huma-num.fr/">
+          https://map-polytheisms.huma-num.fr/
+        </a>
+        ) a permis de collecter et enregistrer à ce stade plus de 27.000
+        attestations de noms divins en langue grecque et en langues sémitiques
+        (hébreu, phénicien, punique, araméen), attestés sur tout le pourtour de
+        la Méditerranée, de l’Iran à la Grande-Bretagne, du Danube au Nil, entre
+        1000 av. et 400 de notre ère.
+        <br />
+        <br />
+        Ces noms, ou séquences onomastiques divines, sont plus ou moins longs,
+        essentiellement entre 2, parfois même 1 seul élément, et 143 éléments
+        mis bout à bout pour la séquence la plus longue. Ces séquences peuvent
+        se référer à une seule divinité ou à plusieurs, jusqu’à 29 ! C’est dire
+        que l’on a affaire à un langage très riche et diversifié, donc complexe.
+        Certains éléments sont très communs et largement partagés – « grand », «
+        seigneur », « sauveur »… – ; d’autres sont la propriété exclusive d’une
+        divinité. Les noms esquissent des portraits très intéressants des
+        divinités : leurs fonctions et modalités d’action, les lieux qu’ils
+        fréquentent, les objets ou animaux qui leur sont associés…
+        <br />
+        <br />
+        Accessible à toutes et tous, et bilingue français/anglais, la base de
+        données MAP (
+        <a href="https://base-map-polytheisms.huma-num.fr/">
+          https://base-map-polytheisms.huma-num.fr/
+        </a>
+        ) constitue un outil unique au monde, qui donne à voir la richesse
+        inouïe des polythéismes, mais aussi d’un monothéisme dans le domaine de
+        l’onomastique divine, c’est-à-dire la manière de nommer les dieux au
+        moyen de noms propres, d’adjectifs et de substantifs, de formes verbales
+        ou adverbiales, entre le respect des traditions et la capacité
+        d’innover. Connaissez-vous « Aphrodite Guide du peuple », ou « Dionysos
+        Luxuriant Berger », ou encore « Roi Hoter Miskar Prince des jours Maître
+        de la terreur » ?
+        <br />
+        <br />
+        L'Atlas numérique des noms divins en Méditerranée est conçu pour vous
+        aider à explorer le foisonnant monde des dieux et des déesses antiques.
+        Il vous propose <strong>deux fonctionnalités principales :</strong>
+        <br />
+        <ul>
+          <li>
+            <strong>« Explorer »</strong> qui vous permet d'accéder à une carte
+            contenant tous les noms divins issus de la base de données MAP et de
+            personnaliser la visualisation des données à travers un systèmes
+            filtres. Vous pourrez ainsi afficher les documents se référant à un
+            lieu, une période, un élément onomastique particulier ou
+            l’association de deux éléments… bref d’explorer les mondes divins à
+            travers des cartes que vous créerez pour répondre aux questions que
+            vous vous posez.
+          </li>
+          <br />
+          <li>
+            <strong>« Découvrir »</strong> qui donne accès à des cartes
+            préconstruites par nos soins pour visualiser tel ou tel phénomène ou
+            processus, et de récits cartographiques ou <em>storymaps</em>, qui
+            mettent en récit au moyen de textes, cartes et images, un aspect de
+            monde divin tel qu’on peut l’appréhender à travers des noms : une
+            divinité particulière, un sanctuaire, une fonction, des attributs…
+            Cette partie propose une architecture par catégories thématiques
+            (les « étiquettes »), tandis que des mots-clés permettent de
+            s’orienter aisément dans les <em>storymaps</em> proposées. Dès lors
+            que nous visons un public international, ces récits pourront être en
+            français, anglais et italien.
+          </li>
+        </ul>
+      </p>
+    </>
+  );
+}
+
+export default HomePageDescription;
